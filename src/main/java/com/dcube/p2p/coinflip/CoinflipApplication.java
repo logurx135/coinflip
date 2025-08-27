14e31581fda11c3878913c74885e8caa8b03acb2
Loganathan Nataraj <50568049+rx135@users.noreply.github.com>
2025-08-27 18:53:01 +0530

    straight from spring starter

---
+++ src/main/java/com/dcube/p2p/coinflip/CoinflipApplication.java
@@ -0,0 +1,13 @@
+package com.dcube.p2p.coinflip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinflipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinflipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcube/p2p/coinflip/CoinflipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcube.p2p.coinflip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinflipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
